f92d025f3ad36308d589fbcd8cc5bcf80b71a5cc
Lee HuiGwang <lhk9805@gmail.com>
2024-10-30 05:03:05 +0900

    feat: 컨버팅 확장 함수 생성
    -Coin.toCoinUi()
    -Double.toDisplayNumber()
    
    issue: #1

--- app/src/main/java/com/winteryy/cryptotracker/crypto/presentation/models/CoinUi.kt
+++ app/src/main/java/com/winteryy/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -1,6 +1,10 @@
 package com.winteryy.cryptotracker.crypto.presentation.models
 
 import androidx.annotation.DrawableRes
+import com.winteryy.cryptotracker.crypto.domain.Coin
+import com.winteryy.cryptotracker.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale
 
 data class CoinUi(
     val id: String,
@@ -17,3 +21,27 @@ data class DisplayableNumber(
     val value: Double,
     val formatted: String
 )
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        priceUsd = priceUsd.toDisplayableNumber(),
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
