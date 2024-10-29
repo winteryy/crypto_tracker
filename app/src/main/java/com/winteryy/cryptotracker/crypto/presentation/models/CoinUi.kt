ecb20d5796747d2d5da7e0d61b1ed4b0dc9ff2af
Lee HuiGwang <lhk9805@gmail.com>
2024-10-30 04:46:44 +0900

    feat: Coin Model 및 Ui Model 정의
    issue: #1

---
+++ app/src/main/java/com/winteryy/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -0,0 +1,19 @@
+package com.winteryy.cryptotracker.crypto.presentation.models
+
+import androidx.annotation.DrawableRes
+
+data class CoinUi(
+    val id: String,
+    val rank: Int,
+    val name: String,
+    val symbol: String,
+    val marketCapUsd: DisplayableNumber,
+    val priceUsd: DisplayableNumber,
+    val changePercent24Hr: DisplayableNumber,
+    @DrawableRes val iconRes: Int
+)
+
+data class DisplayableNumber(
+    val value: Double,
+    val formatted: String
+)
