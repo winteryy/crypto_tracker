113d761d2435672e778178d8a0f994f67817df65
Lee HuiGwang <lhk9805@gmail.com>
2024-10-31 23:58:46 +0900

    feat: 에러 핸들링 로직 작성
    - NetworkError 클래스 바로 문자열 변환할 수 있도록 설정
    - Event용 Channel 생성
    
    issue: #5

--- app/src/main/java/com/winteryy/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/winteryy/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -1,5 +1,6 @@
 package com.winteryy.cryptotracker.crypto.presentation.coin_list
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,19 +12,51 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import com.winteryy.cryptotracker.core.presentation.util.toString
 import com.winteryy.cryptotracker.crypto.presentation.coin_list.components.CoinListItem
 import com.winteryy.cryptotracker.crypto.presentation.coin_list.components.previewCoin
 import com.winteryy.cryptotracker.ui.theme.CryptoTrackerTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.withContext
 
 @Composable
 fun CoinListScreen(
     state: CoinListState,
+    events: Flow<CoinListEvent>,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(lifecycleOwner.lifecycle) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            withContext(Dispatchers.Main.immediate) {
+                events.collect { event ->
+                    when(event) {
+                        is CoinListEvent.Error -> {
+                            Toast.makeText(
+                                context,
+                                event.error.toString(context),
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     if(state.isLoading) {
         Box(
             modifier = modifier
@@ -60,6 +93,7 @@ private fun CoinListScreenPreview() {
                     previewCoin.copy(id = it.toString())
                 }
             ),
+            events = emptyFlow(),
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.background)
         )
--- app/src/main/java/com/winteryy/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/winteryy/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -6,9 +6,11 @@ import com.winteryy.cryptotracker.core.domain.util.onError
 import com.winteryy.cryptotracker.core.domain.util.onSuccess
 import com.winteryy.cryptotracker.crypto.domain.CoinDataSource
 import com.winteryy.cryptotracker.crypto.presentation.models.toCoinUi
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -25,6 +27,9 @@ class CoinListViewModel(
             SharingStarted.WhileSubscribed(5000L),
             CoinListState()
         )
+
+    private val _events = Channel<CoinListEvent>()
+    val events = _events.receiveAsFlow()
 
     fun onAction(action: CoinListAction) {
         when(action) {
@@ -52,6 +57,7 @@ class CoinListViewModel(
                 }
                 .onError { error ->
                     _state.update { it.copy(isLoading = false) }
+                    _events.send(CoinListEvent.Error(error))
                 }
         }
     }
