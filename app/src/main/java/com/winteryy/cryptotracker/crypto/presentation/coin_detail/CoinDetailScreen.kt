3a98a00dc5bee742653f9e51086476a085bd4415
Lee HuiGwang <lhk9805@gmail.com>
2024-11-01 21:49:05 +0900

    feat: CoinDetailScreen 초기 구현
    issue: #7

---
+++ app/src/main/java/com/winteryy/cryptotracker/crypto/presentation/coin_detail/CoinDetailScreen.kt
@@ -0,0 +1,97 @@
+package com.winteryy.cryptotracker.crypto.presentation.coin_detail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.winteryy.cryptotracker.crypto.presentation.coin_list.CoinListState
+import com.winteryy.cryptotracker.crypto.presentation.coin_list.components.previewCoin
+import com.winteryy.cryptotracker.ui.theme.CryptoTrackerTheme
+
+@Composable
+fun CoinDetailScreen(
+    state: CoinListState,
+    modifier: Modifier = Modifier
+) {
+    val contentColor = if(isSystemInDarkTheme()) {
+        Color.White
+    } else {
+        Color.Black
+    }
+    if(state.isLoading) {
+        Box(
+            modifier = modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else if(state.selectedCoin != null) {
+        val coin = state.selectedCoin
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(
+                    id = coin.iconRes
+                ),
+                contentDescription = coin.name,
+                modifier = Modifier.size(100.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+            Text(
+                text = coin.name,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Black,
+                textAlign = TextAlign.Center,
+                color = contentColor
+            )
+            Text(
+                text = coin.symbol,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Light,
+                textAlign = TextAlign.Center,
+                color = contentColor
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun CoinDetailScreenPreview() {
+    CryptoTrackerTheme {
+        CoinDetailScreen(
+            state = CoinListState(
+                selectedCoin = previewCoin
+            ),
+            modifier = Modifier.background(
+                MaterialTheme.colorScheme.background
+            )
+        )
+    }
+}
